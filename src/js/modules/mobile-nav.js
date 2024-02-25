5c02ef093e87cbc9a5e3cfb9fe8463a1b49b3e97
AndriiNS <andriy.naumov05@gmail.com>
2024-02-25 17:43:50 +0200

    +news

--- src/js/modules/mobile-nav.js
+++ src/js/modules/mobile-nav.js
@@ -1,14 +1,26 @@
 function mobileNav() {
-	// Mobile nav button
-	const navBtn = document.querySelector('.mobile-nav-btn');
-	const nav = document.querySelector('.mobile-nav');
-	const menuIcon = document.querySelector('.nav-icon');
+  // Mobile nav button
+  const navBtn = document.querySelector(".mobile-nav-btn");
+  const nav = document.querySelector(".mobile-nav");
+  const menuIcon = document.querySelector(".nav-icon");
 
-	navBtn.onclick = function () {
-		nav.classList.toggle('mobile-nav--open');
-		menuIcon.classList.toggle('nav-icon--active');
-		document.body.classList.toggle('no-scroll');
-	};
+  // Add click event to mobile nav button
+  navBtn.onclick = function () {
+    nav.classList.toggle("mobile-nav--open");
+    menuIcon.classList.toggle("nav-icon--active");
+    document.body.classList.toggle("no-scroll");
+  };
+
+  // Add click event to nav links
+  const navLinks = document.querySelectorAll(".mobile-nav__list a");
+  navLinks.forEach((link) => {
+    link.addEventListener("click", function () {
+      // Close mobile nav after clicking a link
+      nav.classList.remove("mobile-nav--open");
+      menuIcon.classList.remove("nav-icon--active");
+      document.body.classList.remove("no-scroll");
+    });
+  });
 }
 
 export default mobileNav;
